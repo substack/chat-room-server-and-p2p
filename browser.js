bfeeb625bca69f1da328d07a555238f1cd2f6515
James Halliday <mail@substack.net>
2016-05-26 09:15:49 +0200

    webrtc chat

--- browser.js
+++ browser.js
@@ -1,8 +1,28 @@
-var wsock = require('websocket-stream')
 var split = require('split2')
 var through = require('through2')
 var html = require('yo-yo')
+var onend = require('end-of-stream')
 var root = document.querySelector('#content')
+
+var wswarm = require('webrtc-swarm')
+var signalhub = require('signalhub')
+var swarm = wswarm(signalhub('chat-test', [
+  'https://signalhub.mafintosh.com'
+]))
+var peers = {}
+swarm.on('peer', function (peer, id) {
+  peers[id] = peer
+  onend(peer, function () {
+    delete peers[id]
+  })
+
+  peer.pipe(split()).pipe(through(write))
+  function write (buf, enc, next) {
+    state.lines.push(buf.toString())
+    update()
+    next()
+  }
+})
 
 var state = { lines: [] }
 update()
@@ -22,16 +42,11 @@ function render () {
   </div>`
   function onsubmit (ev) {
     ev.preventDefault()
-    stream.write(this.elements.msg.value + '\n')
+    var msg = this.elements.msg.value + '\n'
+    Object.keys(peers).forEach(function (id) {
+      peers[id].write(msg)
+    })
     this.reset()
   }
 }
 
-var stream = wsock('ws://' + location.host)
-stream.pipe(split()).pipe(through(write))
-
-function write (buf, enc, next) {
-  state.lines.push(buf.toString())
-  update()
-  next()
-}
--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-var split = require('split2')
-var through = require('through2')
-var wsock = require('websocket-stream')
-var onend = require('end-of-stream')
 var http = require('http')
 var ecstatic = require('ecstatic')
 var st = ecstatic('public')
@@ -10,19 +6,3 @@ var server = http.createServer(function (req, res) {
   st(req, res)
 })
 server.listen(5000)
-
-var streams = []
-wsock.createServer({ server: server }, function (stream) {
-  streams.push(stream)
-  onend(stream, function () {
-    var ix = streams.indexOf(stream)
-    streams.splice(ix, 1)
-  })
-  stream.pipe(split()).pipe(through(write))
-  function write (buf, enc, next) {
-    streams.forEach(function (stream) {
-      stream.write(buf + '\n')
-    })
-    next()
-  }
-})
